67a00cfd3b0f0b495059ded24c8685fcfa1500bb
AvishkarDalvi <55877429+AvishkarDalvi@users.noreply.github.com>
2019-10-31 11:18:25 +0530

    first commit till state tutorial

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, i'm a React app.</h1>
+        <p>This is really working.</p>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="26"/>
+      </div>
+    );
+    //return React.createElement('div',{className:'App'},React.createElement('h1',{className:'App'},'Does this work now?'));
+  }
+}
+
+export default App;
