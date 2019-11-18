9263f522a40c585b4d29597bc9b3272a90391e79
AvishkarDalvi <55877429+AvishkarDalvi@users.noreply.github.com>
2019-11-18 10:08:07 +0530

    commit

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -13,11 +13,11 @@ class App extends Component {
     otherState:'some other value'
   }
 
-  switchNameHandler=()=>{
+  switchNameHandler=(name)=>{
    // console.log('Was Clicked!!');
   //Dont do this-> this.state.persons[0].name='Maximilian';
   this.setState({persons:[
-    {name:'Maximilian',age:28},
+    {name:name,age:28},
     {name:'Manu',age:29},
     {name:'Stephanie',age:27}
   ] })
@@ -28,10 +28,19 @@ class App extends Component {
       <div className="App">
         <h1>Hi, i'm a React app.</h1>
         <p>This is really working.</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        {/* one way of binding argument to a method call */}
+        <button onClick={()=> this.switchNameHandler('!Maximilian!')}>Switch Name</button>
+        <Person
+         name={this.state.persons[0].name}
+         age={this.state.persons[0].age}/>
+        <Person
+         name={this.state.persons[1].name}
+         age={this.state.persons[1].age}
+         //another way of binding argument to a method call
+         click={this.switchNameHandler.bind(this,'Max!!!')}>My Hobbies: Racing</Person>
+        <Person
+         name={this.state.persons[2].name}
+         age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1',{className:'App'},'Does this work now?'));
