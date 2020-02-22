705ab59e5d9e4a7c082d0425e155384bd0a61458
AvishkarDalvi <55877429+AvishkarDalvi@users.noreply.github.com>
2020-02-22 22:40:23 +0530

    conditional display changes

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,43 +4,54 @@ import Person from './Person/Person'
 
 class App extends Component {
 
-  state={
-    persons:[
-      {name:'Max',age:28},
-      {name:'Manu',age:29},
-      {name:'Stephanie',age:26}
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
     ],
-    otherState:'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
-  switchNameHandler=(name)=>{
-   // console.log('Was Clicked!!');
-  //Dont do this-> this.state.persons[0].name='Maximilian';
-  this.setState({persons:[
-    {name:name,age:28},
-    {name:'Manu',age:29},
-    {name:'Stephanie',age:27}
-  ] })
+  switchNameHandler = (name) => {
+    // console.log('Was Clicked!!');
+    //Dont do this-> this.state.persons[0].name='Maximilian';
+    this.setState({
+      persons: [
+        { name: name, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
   }
-
+  togglePersonsHandler = () => {
+    const show = this.state.showPersons;
+    this.setState({
+      showPersons: !show
+    })
+  }
   render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {
+            this.state.persons.map(person => {
+              return <Person name={person.name} age={person.age} />
+            })
+          }
+        </div>
+      )
+    }
     return (
       <div className="App">
         <h1>Hi, i'm a React app.</h1>
         <p>This is really working.</p>
         {/* one way of binding argument to a method call */}
-        <button onClick={()=> this.switchNameHandler('!Maximilian!')}>Switch Name</button>
-        <Person
-         name={this.state.persons[0].name}
-         age={this.state.persons[0].age}/>
-        <Person
-         name={this.state.persons[1].name}
-         age={this.state.persons[1].age}
-         //another way of binding argument to a method call
-         click={this.switchNameHandler.bind(this,'Max!!!')}>My Hobbies: Racing</Person>
-        <Person
-         name={this.state.persons[2].name}
-         age={this.state.persons[2].age}/>
+        <button onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+
       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1',{className:'App'},'Does this work now?'));
