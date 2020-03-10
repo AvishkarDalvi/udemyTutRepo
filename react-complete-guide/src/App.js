2a36fa9bb7f63b75dd383f2b02c12fa41fa952cd
AvishkarDalvi <55877429+AvishkarDalvi@users.noreply.github.com>
2020-03-10 20:38:51 +0530

    css modules part 2

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 
 import Person from './Person/Person';
 
@@ -43,19 +43,8 @@ class App extends Component {
     })
   }
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
     let persons = null;
+    let btnClass=null;
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -69,26 +58,22 @@ class App extends Component {
           }
         </div>
       );
-       /* style.backgroundColor = 'red';
-      style[':hover'].backgroundColor = {
-        backgroundColor: 'blue',
-        color: 'black'
-      } */
+      btnClass=classes.Red;
     }
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length < 2) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
     return (
 
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, i'm a React app.</h1>
-        <p className={classes.join(' ')}>This is really working.</p>
+        <p className={assignedClasses.join(' ')}>This is really working.</p>
         {/* one way of binding argument to a method call */}
-        <button className='button'
+        <button className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
 
